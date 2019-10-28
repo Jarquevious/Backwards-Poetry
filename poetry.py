00c1b061449f600764afc3a9fb48bd8324a0cdb7
Jarquevious <jarquevious@gmail.com>
2019-10-28 16:32:56 -0700

    added two functions

--- poetry.py
+++ poetry.py
@@ -19,3 +19,16 @@ def lines_printed_backwards(poem_lines_list):
     lines out in reverse 
     with the line numbers reversed.'''
     pass
+
+def lines_printed_random():
+    ''' Your code should implement the lines_printed_random() 
+    function which will randomly select lines from a list of 
+    strings and print them out in random order. Repeats are 
+    okay and the number of lines printed should be equal 
+    to the original number of lines in the poem 
+    (line numbers don't need to be printed). 
+    Hint: try using a loop and randint()  '''
+    pass
+ 
+def my_own_rearrange():
+    pass 
